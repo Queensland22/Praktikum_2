d839c0f502b127834c8a84b5e58c2ece0ccde1cf
Zufar Ramadhan <129580820+Queensland22@users.noreply.github.com>
2024-03-27 22:37:26 +0700

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,10 @@
+public class Main {
 
+    public static void main(String[] args) {
+        PasswordStore pass1 = new PasswordStore("Akun BCA", "dinarC4ndy", "18271628*&adDeh");
+        pass1.setCategory(PasswordStore.CAT_WEBAPP);
+
+        System.out.println(pass1);
+    }
+
+}
